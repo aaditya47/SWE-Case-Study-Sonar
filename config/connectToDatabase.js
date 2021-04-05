538377ef15444eb8120f2c208f6961636d1730d7
aaditya47 <50749476+aaditya47@users.noreply.github.com>
2021-04-05 10:57:43 +0530

    Add files via upload

---
+++ config/connectToDatabase.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const connectToDatabase = async () => {
+    try {
+        const connection=await mongoose.connect(
+            process.env.ATLAS_URI,
+            {
+                useCreateIndex: true,
+                useFindAndModify: true,
+                useUnifiedTopology: true,
+                useNewUrlParser: true
+            }
+        )
+        console.log('Conneceted to DB')
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+module.exports=connectToDatabase;
